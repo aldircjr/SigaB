ba2448276f6ec1ae57cfae3062ca5f4b7d095460
aldir <aldircjr@hotmail.com>
2018-06-26 22:08:02 -0300

    sql funcionando

--- sigab/settings.py
+++ sigab/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'nat',
         'USER': 'django',
-        'PASSWORD': '',
+        'PASSWORD': '123456',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
